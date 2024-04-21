fbf9c875cdbf50e233003af193c81cf261f6a970
ItsBasitAliShah <162181637+ItsBasitAliShah@users.noreply.github.com>
2024-04-21 14:34:49 +0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 #! /usr/bin/env node
-
 //imported Inquirer module, which provides cli interface for node.js
 import inquirer from "inquirer";
 import chalk from "chalk";
-
 //Declared a variable assigned it an inquirer.prompt function
-const answers: {
-  Sentence: string;
-} = await inquirer.prompt([
-  {
-    name: "Sentence",
-    type: "input",
-    message: "Enter your Text: "
-  },
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your Text: "
+    },
 ]);
-
 const words = answers.Sentence.trim().split(" ");
-
 //prints the array of words to console
 console.log(words);
-
 //prints the 
 console.log(chalk.bgBlue("Total Words in your text are " + words.length));
